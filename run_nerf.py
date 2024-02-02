2bab91ab98dd494adcf8a49e543598c58a3e3ded
sseung45 <sseung45@postech.ac.kr>
2024-02-02 19:44:06 +0900

    set device to weights computation

--- run_nerf.py
+++ run_nerf.py
@@ -300,7 +300,7 @@ def raw2outputs(device, raw, z_vals, rays_d, raw_noise_std=0, white_bkgd=False, 
     noise = noise.to(device)
     alpha = raw2alpha(raw[...,3] + noise, dists)  # [N_rays, N_samples]
     # weights = alpha * tf.math.cumprod(1.-alpha + 1e-10, -1, exclusive=True)
-    weights = alpha * torch.cumprod(torch.cat([torch.ones((alpha.shape[0], 1)), 1.-alpha + 1e-10], -1), -1)[:, :-1]
+    weights = alpha * torch.cumprod(torch.cat([torch.ones((alpha.shape[0], 1)), 1.-alpha + 1e-10], -1), -1).to(device)[:, :-1]
     rgb_map = torch.sum(weights[...,None] * rgb, -2)  # [N_rays, 3]
 
     depth_map = torch.sum(weights * z_vals, -1)
