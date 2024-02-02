d4b4cef75d7f216f1fd0f840ad4f9fe706369c58
sseung45 <sseung45@postech.ac.kr>
2024-02-02 19:00:53 +0900

    modify get_rays

--- run_nerf_helpers.py
+++ run_nerf_helpers.py
@@ -150,10 +150,14 @@ class NeRF(nn.Module):
 
 
 # Ray helpers
-def get_rays(H, W, K, c2w, device):
+def get_rays(H, W, K, c2w, device = -1):
     i, j = torch.meshgrid(torch.linspace(0, W-1, W), torch.linspace(0, H-1, H))  # pytorch's meshgrid has indexing='ij'
-    i = i.to(device)
-    j = j.to(device)
+    if device != -1:
+        i = i.to(device)
+        j = j.to(device)
+    else:
+        i = i.t()
+        j = j.t()
     dirs = torch.stack([(i-K[0][2])/K[0][0], -(j-K[1][2])/K[1][1], -torch.ones_like(i)], -1)
     # Rotate ray directions from camera frame to the world frame
     rays_d = torch.sum(dirs[..., np.newaxis, :] * c2w[:3,:3], -1)  # dot product, equals to: [c2w.dot(dir) for dir in dirs]
