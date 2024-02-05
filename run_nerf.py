8a21091ec19ef5f7fdb4e7d31e4b159ea44e6b78
sseung45 <sseung45@postech.ac.kr>
2024-02-05 13:35:53 +0900

    print rays_o, d per device

--- run_nerf.py
+++ run_nerf.py
@@ -98,16 +98,6 @@ def render(device, H, W, K, chunk=1024*32, rays=None, c2w=None, ndc=True,
     if c2w is not None:
         # special case to render full image
         rays_o, rays_d = get_rays(H, W, K, c2w, device)
-        print("origin rays_o: ", len(rays_o))
-        print("origin rays_d: ", len(rays_d))
-        if device == 0:
-            rays_o, _ = np.split(rays_o, 2)
-            rays_d, _ = np.split(rays_d, 2)
-        else:
-            _, rays_o = np.split(rays_o, 2)
-            _, rays_d = np.split(rays_d, 2)
-        print("rank", device, "rays_o: ", len(rays_o))
-        print("rank", device, "rays_d: ", len(rays_d))
     else:
         # use provided ray batch
         rays_o, rays_d = rays
@@ -144,6 +134,9 @@ def render(device, H, W, K, chunk=1024*32, rays=None, c2w=None, ndc=True,
     k_extract = ['rgb_map', 'disp_map', 'acc_map']
     ret_list = [all_ret[k] for k in k_extract]
     ret_dict = {k : all_ret[k] for k in all_ret if k not in k_extract}
+
+    print(device," rays_o: ", len(rays_o))
+    print(device," rays_d: ", len(rays_d))
     return ret_list + [ret_dict]
 
 
