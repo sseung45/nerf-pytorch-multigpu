aef85ccd5292dd219e33d052a3f98ce6df3b4197
sseung45 <sseung45@postech.ac.kr>
2024-02-06 13:24:15 +0900

    convert image reading

--- run_nerf.py
+++ run_nerf.py
@@ -32,11 +32,9 @@ loss_fn = LPIPS(net='vgg')
 
 
 def cal_psnr(gt_tensor, image_path):
-    image = cv2.imread(image_path)
-    gt = np.array(gt_tensor.to('cpu'))
-    gt = cv2.cvtColor(gt, cv2.COLOR_BGR2RGB)
+    image = imageio.imread(image_path)
     print(image, image.dtype)
-    print(gt, gt.dtype)
+    print(gt_tensor, gt_tensor.dtype)
     return 0
 
 def cal_ssim(gt_tensor, image_path):
