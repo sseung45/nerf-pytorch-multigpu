e7987dd8afd9b10d174a3ad7be6806890da81ed4
sseung45 <sseung45@postech.ac.kr>
2024-02-06 15:57:02 +0900

    debug lpips

--- run_nerf.py
+++ run_nerf.py
@@ -920,9 +920,7 @@ def train(rank, world_size):
                                                         **render_kwargs_test)
                     psnr_test += mse2psnr(img2mse(rgb, target))
                     ssim_test += ssim(target, rgb)
-                    print("target shape: ", target.shape)
-                    print("rgb shape: ", rgb.shape)
-                    lpips_test += lpips(target, rgb, net_type='vgg')
+                    lpips_test += lpips(target, target, net_type='vgg')
                 
                 len_test = len(i_val)
                 psnr_test /= len_test
